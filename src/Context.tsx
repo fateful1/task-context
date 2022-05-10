4b6e740f1ea2b98b1fe00a9a6bd26c771bf8f805
fateful1 <facelessasha@yandex.ru>
2022-05-10 13:33:37 +0500

    task-context

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,10 +1,17 @@
-import { ReactElement } from 'react';
+import { createContext, ReactElement, useContext } from 'react';
 
 type Theme = 'light' | 'dark';
 
-export function ThemeProvider(props: {
-    theme: Theme;
-    children: ReactElement;
-}) {}
+const ThemeContext = createContext<Theme>('light');
 
-export function useTheme(): Theme {}
+export function ThemeProvider(props: { theme: Theme; children: ReactElement }) {
+    return (
+        <ThemeContext.Provider value={props.theme}>
+            {props.children}
+        </ThemeContext.Provider>
+    );
+}
+
+export function useTheme(): Theme {
+    return useContext(ThemeContext);
+}
